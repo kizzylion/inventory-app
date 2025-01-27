088c8c8de63edea3d221efa1b1f928658ac293b0
Chukwuma Kizito Iheanacho <kztchm@gmail.com>
2025-01-27 15:53:04 +0100

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -3,12 +3,20 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
+
